5504621f9028a79f7b08baeb01da58fabceac8b9
Duong <tonbao0803@gmail.com>
2024-06-08 15:08:15 +0700

    fix: Change deployment baseURL

--- src/config/axiosClient.ts
+++ src/config/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://fmilk-server.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
